86611c5ffd3febea123b91245d5c7ddbf5d941f1
Tom <->
2024-04-24 01:05:44 +0200

    Initial Commit

---
+++ AirQuality/AirQualityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirQualityApp.swift
+//  AirQuality
+//
+//  Created by Tomasz Kukułka on 24/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct AirQualityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
