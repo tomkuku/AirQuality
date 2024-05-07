1fb46d9f8aa091cae3310ea40ddb0bb8a2067efb
Tom <->
2024-05-07 11:43:28 +0200

    check test

--- .ci-cd/build_and_test.mk
+++ .ci-cd/build_and_test.mk
@@ -9,11 +9,23 @@ PROJECT = AirQuality.xcodeproj
 SCHEME = Development
 PLATFORM = 'iOS Simulator'
 DEVICE = 'iPhone 15 Pro'
-IOS_VERSION = 17.4
+IOS_VERSION = 17.0.1
 CODE_SIGNING_ALLOWED = NO
 
 # - Targets
-all: build_and_test
+all: prepare_environemnt build_and_test
+
+prepare_environemnt:
+	@echo "ℹ️ Reseting simulators"
+	@# Shutdown all devices and boot again to be sure that right simulator is booted
+	@xcrun simctl shutdown all
+	@xcrun simctl boot $(DEVICE)
+	@# Uninstall app and erase simulator to avoid error while tests
+	@xcrun simctl shutdown $(DEVICE)
+	@xcrun simctl erase $(DEVICE)
+	@echo "ℹ️ Removing DerivedData" 
+	@rm -rf ~/Library/Developer/Xcode/DerivedData
+	@rm -rf $(PROJECT)/project.xcworkspace/xcshareddata/swiftpm/Package.resolved
 
 build_and_test:
 	@echo "ℹ️ Building and Testing"
@@ -21,5 +33,4 @@ build_and_test:
 	clean test \
 	-project $(PROJECT) \
 	-scheme $(SCHEME) \
-	-destination platform=$(PLATFORM),name=$(DEVICE),OS=$(IOS_VERSION) \
-	CODE_SIGNING_ALLOWED=$(CODE_SIGNING_ALLOWED)
+	-destination platform=$(PLATFORM),name=$(DEVICE),OS=$(IOS_VERSION)
