4af28dce0a859d785e65b58dce220ae9241802b8
Tom <->
2025-11-06 13:25:00 +0100

    Fixed tests

--- AirQualityTests/PresentationLayer/SensorArchivalMeasurementsListViewModelTests.swift
+++ AirQualityTests/PresentationLayer/SensorArchivalMeasurementsListViewModelTests.swift
@@ -86,8 +86,8 @@ final class SensorArchivalMeasurementsListViewModelTests: BaseTestCase, @uncheck
             .store(in: &cancellables)
         
         // When
-        // Wait for getParameters and setParameters.
-        try await Task.sleep(nanoseconds: 100_000_000)
+        // Wait for getParameters and setParameters and getStream.
+        try await Task.sleep(nanoseconds: 500_000_000)
         sut.fetchTheFirstPage()
         
         // Then
